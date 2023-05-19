f1624c82fe7b97d7da7e41b77ccda70c1668e7d0
PawanAK <pawanajjark@gmail.com>
2023-05-20 01:21:07 +0530

    Displaying Cocktails

--- src/components/Cocktail.jsx
+++ src/components/Cocktail.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-const Cocktail = () => {
+const Cocktail = ({ image, name, id, info, glass, instructions }) => {
   return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
-  )
-}
+    <article className="cocktail">
+      <div className="img-container">
+        <img src={image} alt={name} />
+      </div>
+      <div>
+        <div className="cocktail-footer">
+          <h3>{name}</h3>
+          <h4>{glass}</h4>
+          <p>{info}</p>
+          <Link to={`/cocktail/${id}`}>
+            <button className="btn btn-primary btn-details ">Details</button>
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
 
-export default Cocktail
+export default Cocktail;
